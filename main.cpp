99eea3223d9ccb39fde9c12d1ea89710cb614f0c
jasonlee <i@jas0n1ee.me>
2013-09-04 10:28:00 +0800

    success to load system.init()

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include"setting.h"
 using namespace std;
-const char *stu_rote="student.dat";
-const char *perf_rote="perf.dat";
+char *stu_rout="student.dat";
+char *perf_rout="perf.dat";
 int main()
 {
+	system_SPMS system;
+	system.set_stu_data_add(&stu_rout);
+	system.set_perf_data_add(&perf_rout);
+	system.init();
+	system.add_student();
+	system.save();
 	return 0;
 }
+
 
--- setting.h
+++ setting.h
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<cstdlib>
 #include<fstream>
 #include<cstring>
 #include<time.h>
@@ -37,13 +38,12 @@ struct  stu
 class man
 {
 	public:
-		man();  
-		virtual ~man(); 
+		man(){}
+		virtual ~man(){}
 		virtual void setname()=0;
 		virtual void setID()=0;
 		virtual void setsex()=0; 
 		virtual void setgrade()=0; 
-		friend student;
 	protected:
 		int ID;
 		bool sex;
@@ -54,8 +54,8 @@ class man
 class stuinfo
 {
 	public:
-		stuinfo();
-		virtual ~stuinfo();
+		stuinfo(){}
+		virtual ~stuinfo(){}
 		virtual void check_course()=0;
 		virtual void check_fail()=0;
 	protected:
@@ -69,33 +69,33 @@ class student:public man,public stuinfo
 	public:
 		student();
 		student(stu p);
+		~student(){}
 		void setname();
 		void setID();
 		void setsex(); 
 		void setgrade(); 
 		void check_course();
 		void check_fail();
-		int returnid();
+		stu returnstruct();
 		friend ostream& operator <<(ostream&, student &);
-		friend performance;
-		friend system_SPMS;
+		friend class performance;
+		friend class system_SPMS;
 };
 class performance
 {
 	public:
 		performance();
 		performance(perf p);
-		~performance();
+		~performance(){}
 		void setcoursename();
 		void setcoursevalue();
 		void setcoursescore();
 		void setstuid();
 		void setsubid();
-		int returnid();
-		short int returnsubid();
-		friend ostream& operator <<(ostream&, student &);
-		friend system_SPMS;
-		friend student;
+		perf returnstruct();
+		friend ostream& operator <<(ostream&, performance&);
+		friend class system_SPMS;
+		friend class student;
 	private:
 		int ID;			//ID linked with student's ID
 		short int sub_id;
@@ -108,19 +108,24 @@ class performance
 class system_SPMS
 {
 	public:
-		system_SPMS();
+		system_SPMS(){}
 		system_SPMS(char*stu,char*perf);
 		void init();
-		void set_stu_data_add(char *);		//set the file address for student information
-		void set_perf_data_add(char *);		//set for performance data address
+		void set_stu_data_add(char **);		//set the file address for student information
+		void set_perf_data_add(char **);		//set for performance data address
 		void add_student();
 		void add_performance();
 		int search_stu(int);
-		friend student;
-		friend performance;
+		bool delete_stu(int);
+		bool delete_perf(int,short int);
+		void print_menu();
+		void save();
+		friend class student;
+		friend class performance;
+		friend ostream& operator <<(ostream&, performance&);
 	private:
-		char * stu_address;
-		char * perf_address;
+		char ** stu_address;
+		char ** perf_address;
 		vector<student> data_stu;
 		vector<performance> data_perf;
 		int id_start;
@@ -174,11 +179,17 @@ void student::setID()
 	{
 		cout<<"Please Input Student's ID Number:";
 		cin>>ID;
+		if((*temp).search_stu(ID))
+		{
+			cout<<"Error:ID already exist\n";
+			getchar();
+			setID();
+		}
 	}
 }	
 void student::setsex()
 {
-	cout<<"Please Choose the Student's Sex : \n 1.Male\t2.Female";
+	cout<<"Please Choose the Student's Sex : \n 1.Male\t2.Female\n";
 	cin>>sex;
 	if(sex!=1&&sex!=0) 
 	{
@@ -202,7 +213,6 @@ void student::setgrade()
 	}
 	else return;
 }
-int student::returnid(){return ID;}
 student::student()
 {
 	setname();
@@ -211,6 +221,43 @@ student::student()
 	setgrade();
 	check_course();
 	check_fail();
+}
+ostream& operator<<(ostream&,student& t)
+{
+	cout<<"Name:\t"<<t.stu_name<<"\nID:\t"<<t.ID;
+	switch(t.sex)
+	{
+	case 1:
+		cout<<"\nSex:\tMale";
+		break;
+	default:
+		cout<<"\nSex:\tFemale";
+	}
+	cout<<"\nGrade:\t"<<t.grade<<"\nTotal Credit:\t"<<t.course_num;
+	switch (t.degree)
+	{
+	case 1:
+		cout<<"\nStatus:\tGraduated";
+		break;
+	case 0:
+		cout<<"\nStatus:\tUndergraduate";
+		break;
+	default:
+		cout<<"\nStatus:\tDrop out";
+		break;
+	}
+	return cout;
+}
+stu student::returnstruct()
+{
+	stu t;
+	t.ID=ID;
+	t.sex=sex;
+	t.grade=grade;
+	t.fail_sum=fail_sum;
+	t.course_num=course_num;
+	strcpy(t.stu_name,stu_name);
+	return t;
 }
 /*---------function for performance-----*/
 performance::performance(perf t)
@@ -222,8 +269,6 @@ performance::performance(perf t)
 	value=t.value;
 	strcpy(course_name,t.course_name);
 }
-int performance::returnid(){return ID;}
-short int performance::returnsubid(){return sub_id;}
 void performance::setcoursename()
 {
 	char t[]="Please Inpute the Course Name";
@@ -262,42 +307,88 @@ void performance::setstuid()
 {
 	cout<<"Please Input Student ID:\n";
 	cin>>ID;
+	if((*temp).search_stu(ID)==0)
+	{
+		cout<<"Student ID incorrect. Please Try Again.\n";
+		getchar();
+		setstuid();
+	}
 }
 void performance::setsubid()
 {
 	cout<<"Please Input Course ID:\n";
 	cin>>sub_id;
 }
-/*---------function for system----------*/
-void system_SPMS::set_stu_data_add(char *t)
+performance::performance()
 {
-	strcpy(stu_address,t);
+	faial_mark=0;
+	setcoursename();
+	setstuid();
+	setsubid();
+	setcoursevalue();
+	setcoursescore();
+	(*temp).data_stu[(*temp).search_stu(ID)].check_course();
+	(*temp).data_stu[(*temp).search_stu(ID)].check_fail();
 }
-void system_SPMS::set_perf_data_add(char *t)
+ostream& operator <<(ostream&,performance &t)
 {
-	strcpy(perf_address,t);
+	cout<<"StudentName:\t"<<(*temp).data_stu[(*temp).search_stu(t.ID)];
+	cout<<"\nCourse ID:\t"<<t.sub_id<<"\nCourse Credit:\t"<<t.value<<"\nScore:\t"<<t.score<<"\nFailMark:\t";
+	if(t.faial_mark) cout<<"Yes\n";
+	else cout<<"No\n";
+	return cout;
+}
+perf performance::returnstruct()
+{
+	perf t;
+	t.ID=ID;
+	t.sub_id=sub_id;
+	t.score=score;
+	t.value=value;
+	t.faial_mark=faial_mark;
+	strcpy(t.course_name,course_name);
+	return t;
+}
+/*---------function for system----------*/
+void system_SPMS::set_stu_data_add(char **t)
+{
+	//strcpy(stu_address,t);
+	stu_address=t;
+}
+void system_SPMS::set_perf_data_add(char **t)
+{
+	//strcpy(perf_address,t);
+	perf_address=t;
 }
 void system_SPMS::init()
 {
 	temp=this;
 	time_t t;
 	stu t_stu;
-	performance t_perf;
+	perf t_perf;
+	int stu_sum,perf_sum;
 	t=time(NULL);
 	id_start=1000*(t/31104000+1969);
-	ifstream infile(stu_address,ios::binary|ios::in);
-	for(;infile.eof();)
+	ifstream infile(*stu_address,ios::binary|ios::in|ios::ate);
+	stu_sum=infile.tellg()/sizeof(stu);
+	infile.seekg(ios::beg);
+	for(int i=0;i<stu_sum;i++)
 	{
 		infile.read((char *)&t_stu,sizeof(stu));
 		data_stu.push_back(student(t_stu));
+		cout<<i+1<<"item(s) read\n";
 	}
 	infile.close();
-	ifstream infile(perf_address,ios::binary|ios::in);
-	for(;infile.eof();)
+	ifstream infile2(*perf_address,ios::binary|ios::in|ios::ate);
+	perf_sum=infile2.tellg()/sizeof(perf);
+	infile2.seekg(ios::beg);
+	for(int j=0;j<perf_sum;j++)
 	{
-		infile.read((char *)&t_perf,sizeof(perf));
-		data_perf.push_back(performance(t_perf));
+		infile2.read((char *)&t_perf,sizeof(perf));
+		//data_perf.push_back(performance(t_perf));
+		cout<<j+1<<"item(s) read\n";
 	}	
+	infile2.close();
 }
 int system_SPMS::search_stu(int t)
 {
@@ -305,11 +396,61 @@ int system_SPMS::search_stu(int t)
 	int j,flag=-1;
 	for(i=data_stu.begin(),j=0;i!=data_stu.end();i++,j++)
 	{
-		if((*i).returnid()==t) flag=j;
+		if((*i).ID==t) flag=j;
 	}
 	return flag;
 }
-
+void system_SPMS::add_student()
+{
+	data_stu.push_back(student());
+}
+void system_SPMS::add_performance()
+{
+	data_perf.push_back(performance());
+}
+bool system_SPMS::delete_stu(int t)
+{
+	bool flag=0;
+	vector<student>::iterator i;
+	for(i=data_stu.begin();i!=data_stu.end();i++)
+	{
+		if((*i).ID==t)
+		{
+			flag=1;
+			cout<<*i<<"\nDeleted!\n";
+			data_stu.erase(i);
+			break;
+		}
+	}
+	return flag;
+}
+bool system_SPMS::delete_perf(int t,short int st)
+{
+	bool flag=0;
+	vector<performance>::iterator i;
+	for(i=data_perf.begin();i!=data_perf.end();i++)
+	{
+		if((*i).ID==t&&(*i).sub_id==st)
+		{
+			flag=1;
+			cout<<*i<<"\nDeleted!\n";
+			data_perf.erase(i);
+			break;
+		}
+	}
+	return flag;
+}
+void system_SPMS::save()
+{
+	vector<student>::iterator i;
+	vector<performance>::iterator j;
+	fstream outfile(*stu_address,ios::out|ios::trunc|ios::binary);
+	for(i=data_stu.begin();i!=data_stu.end();i++) outfile.write((char*)&(*i).returnstruct(),sizeof(stu));
+	outfile.close();
+	fstream outfile2(*perf_address,ios::out|ios::trunc|ios::binary);
+	for(j=data_perf.begin();j!=data_perf.end();j++) outfile2.write((char*)&(*j).returnstruct(),sizeof(perf));
+	outfile2.close();
+}
 #define SETTING
 #endif
 
